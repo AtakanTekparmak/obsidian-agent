5ff552c191e055294b40e4caf9c620e938936ba0
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-04-23 12:51:49 +0200

    fix: Chanegd gemini flash model

--- data/settings.py
+++ data/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 # Gemini
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_FLASH = "gemini-2.0-flash"
+GEMINI_FLASH = "gemini-2.5-flash-preview-04-17"
 GEMINI_PRO = "gemini-2.5-pro-exp-03-25"
 
 # File paths
