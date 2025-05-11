904f1c8b20d4dba28b8e5d09d38eaf9f7c09f4bc
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-05-11 17:17:42 +0200

    feat: Added load kb

--- data/utils.py
+++ data/utils.py
@@ -1,8 +1,11 @@
 import json
 from pathlib import Path
+import os
 
 from pydantic import BaseModel
 
+from data.settings import OUTPUT_PATH, KB_PATH
+from data.schemas.kb import KnowledgeBase
 def save_pydantic_to_json(model: BaseModel, filepath: str) -> None:
     """
     Save a Pydantic model to a JSON file.
@@ -19,4 +22,22 @@ def save_pydantic_to_json(model: BaseModel, filepath: str) -> None:
             f.write(model.model_dump_json(indent=2))
     except IOError as e:
         print(f"Error saving file {filepath}: {e}")
+        raise
+
+def load_kb_from_json(filepath: str = os.path.join(OUTPUT_PATH, KB_PATH, "kb.json")) -> KnowledgeBase:
+    """
+    Load a KnowledgeBase object from a JSON file.
+
+    Args:
+        filepath: The path to the JSON file
+
+    Returns:
+        A KnowledgeBase object
+    """
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+        return KnowledgeBase.model_validate(data)
+    except IOError as e:
+        print(f"Error loading file {filepath}: {e}")
         raise
--- generate_data.py
+++ generate_data.py
@@ -1,11 +1,15 @@
 from data.pipeline import generate_personas, generate_momentary_stories, generate_kb, generate_multiturn_convos
+from data.utils import load_kb_from_json
 
 
 def main():
+    """
     scenario = "Groningen, Netherlands in 2025"
     personas = generate_personas(4, scenario, save=True)
     stories = generate_momentary_stories(8, personas, save=True)
     kb = generate_kb(personas, stories, save=True)
+    """
+    kb = load_kb_from_json()
     multiturn_convos = generate_multiturn_convos(kb, save=True) 
 
 if __name__ == "__main__":
