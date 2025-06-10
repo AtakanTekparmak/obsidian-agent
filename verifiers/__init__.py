a0e7d1d1e0f8e49a54ca0895b82e0b93861e2956
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-06-10 13:00:03 +0200

    fix: Another import fix

--- verifiers/__init__.py
+++ verifiers/__init__.py
@@ -3,4 +3,7 @@ from typing import Callable
 # Export the RewardFunc type for use in rubrics
 RewardFunc = Callable[..., float]
 
+# Re-export everything from verifiers.verifiers for convenience
+from verifiers.verifiers import *
+
 __all__ = ["RewardFunc"]
