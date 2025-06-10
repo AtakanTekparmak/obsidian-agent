716ffe0276fc27242031d15999db203f02d57c35
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-06-10 12:46:41 +0200

    fix: Further fixes

--- verifiers/verifiers/rubrics/__init__.py
+++ verifiers/verifiers/rubrics/__init__.py
@@ -5,7 +5,6 @@ from .math_rubric import MathRubric
 from .codemath_rubric import CodeMathRubric
 from .tool_rubric import ToolRubric
 from .smola_tool_rubric import SmolaToolRubric
-from .retrieval_rubric import get_retrieval_rubric
 
 __all__ = [
     "Rubric",
@@ -14,6 +13,5 @@ __all__ = [
     "MathRubric",
     "CodeMathRubric",
     "ToolRubric",
-    "SmolaToolRubric",
-    "get_retrieval_rubric"
+    "SmolaToolRubric"
 ]
