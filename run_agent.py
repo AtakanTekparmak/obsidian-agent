412730fe8bb719ce77031f162fa5bedd9b14c348
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-06-25 13:53:21 +0200

    fix: Updated system prompt

--- run_agent.py
+++ run_agent.py
@@ -1,17 +1,15 @@
 from agent.agent import Agent
 import uuid
 
-MODEL_NAME = "mistralai/mistral-medium-3"
+MODEL_NAME = "google/gemini-2.5-flash"
 
 def run_agent():
     agent = Agent(
         model=MODEL_NAME,
-        memory_path = f"memory_{uuid.uuid4()}",
-        predetermined_memory_path=True
+        memory_path = "memory/test_ret"
     )
 
-    response = agent.chat("Hard to believe I'm already 28. I'm not sure how I feel about it. What feels good though, is I will have my favourite dessert today: chocolate cake.")
-    response_2 = agent.chat("You know what is great about growing older though? There's less expectations on you.")
+    response = agent.chat("What's the age of my wife?")
     agent.save_conversation(save_folder="new_prompt_test")
     
 if __name__ == "__main__":
