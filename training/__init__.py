113aecf23a7e5cb49b99359e0976d4cb55e8f5f5
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-07-17 17:51:32 +0300

    feat: removed dead code

--- training/__init__.py
+++ training/__init__.py
@@ -1,34 +1,8 @@
 from .reward import dump_folder, get_reward, get_folder_reward
 
-can_import_retrieval = True
 
-try:
-    from .retrieval import (
-        create_kb_with_personas,
-        build_verifiers_dataset,
-        generate_question_prompt,
-        get_retrieval_rubric,
-        RetrievalEnv,
-    )
-    # Import the registration module to register the environment
-    from .retrieval import register_env
-except Exception as _:
-    can_import_retrieval = False
-
-
-if can_import_retrieval:
-    __all__ = [
-        "dump_folder",
-        "get_reward",
-        "get_folder_reward",
-        "create_kb_with_personas",
-        "build_verifiers_dataset",
-        "generate_question_prompt",
-        "get_retrieval_rubric",
-        "RetrievalEnv",
-    ]
-else:
-    __all__ = [
-        "dump_folder",
-        "get_folder_reward",
-    ]
+__all__ = [
+    "dump_folder",
+    "get_folder_reward",
+    "get_reward",
+]
